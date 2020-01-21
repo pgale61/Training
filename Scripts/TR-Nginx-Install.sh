8d7bac21ea76b3046b9ce49f2e6454d1c478aade
pgale61 <pgale61@gmail.com>
2020-01-21 11:20:07 +0000

    Update TR-Nginx-Install.sh

--- Scripts/TR-Nginx-Install.sh
+++ Scripts/TR-Nginx-Install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
  
 sudo apt-get purge nginx nginx-common nginx-full
- 
+
 sudo apt-get update
 sudo apt-get install nginx -y
+sudo apt-get install -y python-setuptools python-pip
+
+pip install jinja2
