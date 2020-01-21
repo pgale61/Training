ffd4ae52398a89e1fdd0685018a003bbbabd4ab5
pgale61 <pgale61@gmail.com>
2020-01-21 11:20:55 +0000

    Update TR-MySQL-Install.sh

--- Scripts/TR-MySQL-Install.sh
+++ Scripts/TR-MySQL-Install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 
 echo 'mysql-server-5.7 mysql-server/root_password password root' > /var/tmp/dss.1
 echo 'mysql-server-5.7 mysql-server/root_password_again password root' > /var/tmp/dss.2
-sudo debconf-set-selections /var/tmp/dss..1
-sudo debconf-set-selections /var/tmp/dss..2
+sudo debconf-set-selections /var/tmp/dss.1
+sudo debconf-set-selections /var/tmp/dss.2
 
 sudo apt-get -y install mysql-server-5.7
